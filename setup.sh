e00dd2787069d6a9cf5d78c1fd0e62e91b5c603c
gram <ganeshram42000@gmail.com>
2022-10-07 02:31:37 +0530

    added simple shell script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+## updating the packages (comment out if using different distro other than debian derivative)
+
+apt-get update && sudo apt-get upgrade
+
+echo "********Installing docker***********"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+echo "************************************"
+echo "********Installing PiHole***********"
+pushd PiHole/
+sudo docker compose up -d
+popd
+echo "************************************"
+echo "******installing Jellyfin***********"
+pushd Jellyfin/
+sudo docker compose up -d
+popd
+echo "*************************************"
+echo "********Installing NextCloud*********"
+pushd Nextcloud/
+sudo docker compose up -d
+popd
+echo "*************************************"
