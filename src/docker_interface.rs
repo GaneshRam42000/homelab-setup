196afb9d62c26e875463be53857253cac17d8a60
ganesh <ganeshram42000@gmail.com>
2026-06-12 22:47:36 +0530

    docker interfacing init code

--- src/docker_interface.rs
+++ src/docker_interface.rs
@@ -1,12 +1,70 @@
-pub trait Docker {
-    fn docker_shell_command(&self);
+use std::process::Command;
+
+pub struct List;
+
+impl List {
+    pub fn run(&self) -> std::io::Result<()> {
+        Command::new("docker").args(["container", "ls"]).status()?;
+        Ok(())
+    }
 }
 
-pub struct List {
-    pub command: String,
+pub struct ComposeStart {
+    pub compose_file_path: String,
+    pub compose_env_path: String,
 }
-impl Docker for List {
-    fn docker_shell_command(&self) {
-        println!("{}", self.command);
+
+impl ComposeStart {
+    pub fn run(&self) -> std::io::Result<()> {
+        Command::new("docker")
+            .args([
+                "compose",
+                "up",
+                "-f",
+                &self.compose_file_path,
+                "--env-file",
+                &self.compose_env_path,
+            ])
+            .status()?;
+        Ok(())
+    }
+}
+
+pub struct ComposeStop {
+    pub compose_file_path: String,
+}
+
+impl ComposeStop {
+    pub fn run(&self) -> std::io::Result<()> {
+        Command::new("docker")
+            .args(["compose", "stop", "-f", &self.compose_file_path])
+            .status()?;
+        Ok(())
+    }
+}
+
+pub struct ComposeRestart {
+    pub compose_file_path: String,
+}
+
+impl ComposeRestart {
+    pub fn run(&self) -> std::io::Result<()> {
+        Command::new("docker")
+            .args(["compose", "restart", "-f", &self.compose_file_path])
+            .status()?;
+        Ok(())
+    }
+}
+
+pub struct ComposePrune {
+    pub compose_file_path: String,
+}
+
+impl ComposePrune {
+    pub fn run(self) -> std::io::Result<()> {
+        Command::new("docker")
+            .args(["container", "prune", "-f", &self.compose_file_path])
+            .status()?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 mod docker_interface;
-use crate::docker_interface::Docker;
 use docker_interface::List;
 
-fn main() {
-    let command = List {
-        command: String::from("docker container ls"),
-    };
-    command.docker_shell_command();
+fn main() -> std::io::Result<()> {
+    let command = List;
+    command.run()?;
+    Ok(())
 }
