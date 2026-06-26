8721d8921e08579d28ce2ff1480e8bb503872daf
ganesh <ganeshram42000@gmail.com>
2026-06-26 11:49:05 +0530

    repo management boiler plate code

--- src/repo_management.rs
+++ src/repo_management.rs
@@ -7,7 +7,7 @@ fn read_or_create_repo_config(path: &str, default_content: &str) -> io::Result<S
         Err(err) if err.kind() == io::ErrorKind::NotFound => {
             let mut file = OpenOptions::new().write(true).create_new(true).open(path)?;
 
-            file.write_all(default_content.as_bytes())?;
+            file.write_all(default_content.as_bytes())?
 
             Ok(default_content.to_string())
         }
